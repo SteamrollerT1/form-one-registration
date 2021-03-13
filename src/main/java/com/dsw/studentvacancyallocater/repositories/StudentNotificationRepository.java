b8ce5861cfa3196b1ce605e57c1cfcd48c16388d
Tendai D. Zhou <tendaidzhou@gmail.com>
2021-03-13 14:08:46 +0200

    Read student notifications

--- src/main/java/com/dsw/studentvacancyallocater/repositories/StudentNotificationRepository.java
+++ src/main/java/com/dsw/studentvacancyallocater/repositories/StudentNotificationRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface StudentNotificationRepository extends JpaRepository<StudentNotification, Long> {
-    @Query("SELECT sn FROM StudentNotification sn JOIN sn.student st WHERE st.id = :studentId")
+    @Query("SELECT sn FROM StudentNotification sn JOIN sn.student st WHERE st.id = :studentId ORDER BY sn.dateCreated")
     List<StudentNotification> getStudentNotificationsByStudentId(@Param("studentId") long studentId);
 }
