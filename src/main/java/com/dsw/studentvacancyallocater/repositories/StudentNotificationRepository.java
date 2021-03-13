51998d4ad8b2503378f3188e20de3bcff96d9d6d
Tendai D. Zhou <tendaidzhou@gmail.com>
2021-03-13 14:09:33 +0200

    Order notification by data created desc

--- src/main/java/com/dsw/studentvacancyallocater/repositories/StudentNotificationRepository.java
+++ src/main/java/com/dsw/studentvacancyallocater/repositories/StudentNotificationRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface StudentNotificationRepository extends JpaRepository<StudentNotification, Long> {
-    @Query("SELECT sn FROM StudentNotification sn JOIN sn.student st WHERE st.id = :studentId ORDER BY sn.dateCreated")
+    @Query("SELECT sn FROM StudentNotification sn JOIN sn.student st WHERE st.id = :studentId ORDER BY sn.dateCreated DESC")
     List<StudentNotification> getStudentNotificationsByStudentId(@Param("studentId") long studentId);
 }
